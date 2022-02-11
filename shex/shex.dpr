2ca740780f6671ce7597adebf4194a6fe6867ef4
Vlad <vsevols@gmail.com>
2022-02-11 14:22:34 +0300

    .freezeAfterFinish=True

--- shex/shex.dpr
+++ shex/shex.dpr
@@ -8,15 +8,26 @@ uses
   System.SysUtils,
   CommandHandlerUnit in 'Units\CommandHandlerUnit.pas';
 
+{
+  * gitProjectWorkingPath
+    shex://d:\Git\shex\shex?
+  * build target
+    shex://d:\Git\shex\shex\Win32\Debug?
+	* deployed
+    shex://x:\programs\shex?
+}
+
 var
   handler: ICommandHandler;
+  freezeAfterFinish: Boolean;
 begin
   try
     WriteLn('received: '+CmdLine);
 
     handler:=TCommandHandler.Create;
     handler.Process(CmdLine);
-//    if False then
+    freezeAfterFinish:=FileExists(ExtractFilePath(ParamStr(0))+'.freezeAfterFinish=True');
+    if freezeAfterFinish then
       ReadLn;
   except
     on E: Exception do
